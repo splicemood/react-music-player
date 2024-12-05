3c7d6441c9a2f9cc4a635d25b3b7312a3c3fa07b
Manazoid <uralkir@gmail.com>
2024-12-05 06:10:34 +0500

    update: github link

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Title } from '@mantine/core';
+import { FaGithub } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
+import { ActionIcon, Group, Title } from '@mantine/core';
 
 const Welcome = () => {
-  return <Title order={3}>Music Player</Title>;
+  return (
+    <Group gap={'xl'}>
+      <Title order={3}>Music Player</Title>
+      <ActionIcon
+        component={Link}
+        to={'https://github.com/splicemood/react-music-player'}
+        variant={'outline'}
+        color={'text'}
+        aria-label="Source code"
+      >
+        <FaGithub size={20} />
+      </ActionIcon>
+    </Group>
+  );
 };
 
 export default Welcome;
